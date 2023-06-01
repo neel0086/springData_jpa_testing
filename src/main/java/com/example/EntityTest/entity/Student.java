d81039d16fe39acf0dc9c34f8fca65a3f33a9896
Neel Mehta <neelmehta0086@gmail.com>
2023-06-01 13:15:29 +0530

    feat:Setup

---
+++ src/main/java/com/example/EntityTest/entity/Student.java
@@ -0,0 +1,26 @@
+package com.example.EntityTest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="Student")
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long studentId;
+    private String name;
+    private String email;
+    private Long age;
+}
